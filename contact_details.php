2b02d02ab401a098859c02ce9363e8a26877e76f
Richard West <r.j.west@live.com.au>
2020-05-03 17:28:36 +1000

    Updated Contact form php file

--- contact_details.php
+++ contact_details.php
@@ -1,8 +1,18 @@
 
 <?php
-	$name = $_Post['name'];
-	$visitor_email = $_Post['email'];
-	$message = $_Post['message']
+	if (isset($_POST['Submit'])) {
+		$name = $_POST['name'];
+		$mail_from = $_POST['email'];
+		$telephone_no = $_POST['phone'];
+		$state = $_POST['state']
+		$subject = "New Inquiry"
+	
+		$mailTo = "r.j.west@live.com.au";
+		$headers = "From: " .$mail_from;
+		$txt = "Phone No.:" .$telephone_no "/n State:" .$state
 
-	$email_form = "r.j.west@live.com.au"
+		mail($mailTo, $subject, $txt);
+		header("Location: Contact.html?mailsend");
+	}
+
 ?>
